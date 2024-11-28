46778f23c65696d7a111c5b15535e56b879a3e6c
gaojj2000 <1930502098@qq.com>
2024-11-28 17:15:00 +0800

    做了小优化以及在main.py中添加了更多的示例

--- Ncatbot/apis/base.py
+++ Ncatbot/apis/base.py
@@ -7,20 +7,6 @@ import aiohttp
 
 
 # Python3.8版本出现 'staticmethod' object is not callable 报错
-def get_media_path(media_path):
-    """
-    获取媒体的本地绝对路径或网络路径
-    """
-    if media_path:
-        if media_path.startswith('http'):
-            return media_path
-        elif os.path.isfile(media_path):
-            abspath = os.path.abspath(os.path.join(os.getcwd(), media_path)).replace('\\', '\\\\')
-            return f"file:///{abspath}"
-    return ''
-
-
-# Python3.8版本出现 'staticmethod' object is not callable 报错
 def replace_none(fun):
     """
     去除 json 参数中为 None 的键值对（装饰器自动操作版）
@@ -47,6 +33,19 @@ class Base:
         self.sync = sync  # 是否使用同步请求
         self.headers = {'Content-Type': 'application/json'}
         self.url = port_or_http if str(port_or_http).startswith('http') else f'http://localhost:{port_or_http}'
+
+    @staticmethod
+    def get_media_path(media_path):
+        """
+        获取媒体的本地绝对路径或网络路径
+        """
+        if media_path:
+            if media_path.startswith('http'):
+                return media_path
+            elif os.path.isfile(media_path):
+                abspath = os.path.abspath(os.path.join(os.getcwd(), media_path)).replace('\\', '\\\\')
+                return f"file:///{abspath}"
+        return ''
 
     @replace_none
     async def get(self, *args, **kwargs):
