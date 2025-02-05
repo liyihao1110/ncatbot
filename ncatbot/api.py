cd14c559ac2f5bdf16fdb315f2807e04ffd5c0db
Niloux3d-MacOS <15072763003@163.com>
2025-02-05 17:52:03 +0800

    通过base64编码传输文件

--- ncatbot/api.py
+++ ncatbot/api.py
@@ -1,4 +1,6 @@
 import os
+import base64
+import mimetypes 
 from typing import Union
 
 from .http import Route, WsRoute
@@ -6,15 +8,37 @@ from .status import Status
 from .utils.mdmaker import md_maker
 
 
+def file_to_data_uri(file_path):
+    # 获取文件的 MIME 类型
+    mime_type, _ = mimetypes.guess_type(file_path)
+    
+    if mime_type is None:
+        raise ValueError("Unable to guess MIME type for the file.")
+    
+    # 打开文件并读取为二进制数据
+    with open(file_path, "rb") as file:
+        file_data = file.read()
+        
+        # 将二进制数据编码为 Base64
+        encoded_data = base64.b64encode(file_data)
+        
+        # 将 Base64 编码的数据转换为字符串
+        base64_str = encoded_data.decode('utf-8')
+        
+        # 构造 data URI
+        data_uri = f"data:{mime_type};base64,{base64_str}"
+        
+        return data_uri
+
 def convert(i, message_type):
     if i.startswith("http"):
         return {"type": message_type, "data": {"file": i}}
     elif i.startswith("base64://"):
         return {"type": message_type, "data": {"file": i}}
     elif os.path.exists(i):
-        return {"type": message_type, "data": {"file": f"file:///{os.path.abspath(i)}"}}
+        return {"type": message_type, "data": {"file": f"{file_to_data_uri(i)}"}}
     else:
-        return {"type": message_type, "data": {"file": f"file:///{i}"}}
+        return {"type": message_type, "data": {"file": f"{i}"}}
 
 
 def read_file(file_path) -> any:
