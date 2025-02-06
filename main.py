5c2c6f457d0eb0bbea8171f4d9255374459d93ef
zxxhz <ou0897@foxmail.com>
2025-02-06 08:55:46 +0800

    revert: 恢复被ai删掉的部分

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ bot = BotClient()
 
 @bot.group_event()
 async def on_group_message(msg: GroupMessage):
+    _log.info(msg)
     if msg.raw_message == "test1":
         # 使用 MessageChain 发送复合消息
         message = MessageChain(
@@ -23,6 +24,7 @@ async def on_group_message(msg: GroupMessage):
 
 @bot.private_event()
 async def on_private_message(msg: PrivateMessage):
+    _log.info(msg)
     if msg.raw_message == "test":
         await bot.api.post_private_msg(
             msg.user_id, text="你好,", face=1, reply=msg.message_id
@@ -31,6 +33,9 @@ async def on_private_message(msg: PrivateMessage):
         await bot.api.add_at(msg.user_id).add_face(1).add_text("你好").add_face(
             2
         ).send_private_msg(msg.user_id, reply=msg.message_id)
+    elif msg.raw_message == "md":
+        await bot.api.post_private_file(user_id=msg.user_id, markdown="README.md")
+        # await bot.api.post_private_msg(msg.user_id, markdown="## 你好")
 
 
 @bot.notice_event
