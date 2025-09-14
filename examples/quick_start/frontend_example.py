84a77b1e8333e4dec28600555fae19ae03f21ebb
huan-yp <3051561876@qq.com>
2025-09-14 12:46:51 +0800

    fix front-end example

--- examples/quick_start/frontend_example.py
+++ examples/quick_start/frontend_example.py
@@ -1,4 +1,4 @@
-from ncatbot.core import BotClient, BaseMessageEvent
+from ncatbot.core import BotClient, BaseMessageEvent, GroupMessageEvent
 from ncatbot.utils import config
 from ncatbot.plugin_system import on_message
 
@@ -13,9 +13,9 @@ async def on_private_message(event: BaseMessageEvent):
     msg = event.message
     if event.is_group_msg():
         if msg.filter_text()[0].text == "测试":
-            event.reply("群聊：前端模式测试成功")
+            await event.reply("群聊：前端模式测试成功")
     else:
-        event.reply("你好呀! 我是 NcatBot!")
+        await event.reply("你好呀! 我是 NcatBot!")
 
 
 bot.run_frontend(debug=True)  # 前台线程启动，返回全局 API（同步友好）
